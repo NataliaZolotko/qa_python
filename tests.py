2317f0808e65432c3cae38181fb1595d52a46c3e
Natalia Zolotko <z0lotkon@yandex.ru>
2025-09-29 00:32:08 +0500

    Sprint_4

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
+import pytest
 from main import BooksCollector
+
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -22,3 +24,24 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    #проверяем добавление книг с валидной длиной названия
+    @pytest.mark.parametrize('name',['А','Моана','Бббббббббббббббббббббббббббббббббббббббб']) 
+    def test_add_new_book_valid_lenght(name):
+        collector = BooksCollector()
+        if 0 < len(name) < 41:
+            collector.add_new_book(name)
+            assert name in collector.books_genre
+
+    #проверяем, что книги не добавляются с невалидной длиной названия
+    @pytest.mark.parametrize('name',[' ','длинноеназваниекотороепревышаетсорокодинс']) 
+    def test_add_new_book_invalid_lenght(name):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        assert name not in collector.books_genre
+        
+    # проверяем, что книге устанавливается жанр
+    def test_set_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Оно')
+        collector.set_book_genre('Оно', 'Ужасы')
+        assert collector.get_book_genre('Оно') == 'Ужасы'
